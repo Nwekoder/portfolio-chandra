291a3acbe4bdc7967ad6ed79b7b9e3ed5d0bc378
Nwekoder <cn1250425@gmail.com>
2023-10-16 12:31:43 +0800

    Updating whole design

--- src/App.tsx
+++ src/App.tsx
@@ -1,238 +1,98 @@
-import { ReactNode } from "react";
-import { Icon } from "@iconify/react/dist/iconify.js";
-
-type SkillCardProps = {
-  icon: ReactNode;
-  name: string;
-  star_rating: number;
-};
-
-const my_skills: SkillCardProps[] = [
-  {
-    icon: <Icon icon="devicon:react" width={90} height={90} />,
-    name: "React",
-    star_rating: 2,
-  },
-  {
-    icon: <Icon icon="logos:nextjs" width={90} height={90} fill="#FFFFFF" />,
-    name: "Next.js",
-    star_rating: 2,
-  },
-  {
-    icon: <Icon icon="logos:svelte-icon" width={90} height={90} />,
-    name: "Svelte",
-    star_rating: 2,
-  },
-  {
-    icon: <Icon icon="logos:javascript" width={90} height={90} />,
-    name: "Javascript",
-    star_rating: 2,
-  },
-  {
-    icon: <Icon icon="vscode-icons:file-type-python" width={90} height={90} />,
-    name: "Python",
-    star_rating: 2,
-  },
-  {
-    icon: <Icon icon="vscode-icons:file-type-html" width={90} height={90} />,
-    name: "HTML",
-    star_rating: 3,
-  },
-  {
-    icon: <Icon icon="devicon:css3" width={90} height={90} />,
-    name: "CSS",
-    star_rating: 3,
-  },
-  {
-    icon: <Icon icon="vscode-icons:file-type-node" width={90} height={90} />,
-    name: "Node.JS",
-    star_rating: 2,
-  },
-  {
-    icon: <Icon icon="logos:laravel" width={90} height={90} />,
-    name: "Laravel",
-    star_rating: 1,
-  },
-  {
-    icon: <Icon icon="logos:sass" width={90} height={90} />,
-    name: "Sass",
-    star_rating: 2,
-  },
-  {
-    icon: <Icon icon="vscode-icons:file-type-less" width={90} height={90} />,
-    name: "Less",
-    star_rating: 3,
-  },
-  {
-    icon: <Icon icon="logos:tailwindcss-icon" width={90} height={90} />,
-    name: "TailwindCSS",
-    star_rating: 2,
-  },
-].sort((a, b) => b.star_rating - a.star_rating);
+import { useRef } from "react";
+import { motion, useTransform, useScroll } from "framer-motion";
 
 export default function App() {
+  const fotoContainerRef = useRef(null);
+
+  const fotoSendiriScroll = useScroll({
+    target: fotoContainerRef,
+    offset: ["center center", "end end"],
+  });
+
+  const transformFotoSendiri = useTransform(
+    fotoSendiriScroll.scrollY,
+    [0, 850],
+    ["0rem", "-15rem"]
+  );
+
   return (
     <main>
-      <div className="foto-sendiri">
-        <img src="/foto_sendiri.png" alt="Foto Sendiri" />
+      <div ref={fotoContainerRef} className="absolute">
+        <motion.img
+          style={{ bottom: transformFotoSendiri }}
+          src="/foto_sendiri.png"
+          alt="foto Chandra"
+          className="foto-sendiri"
+        />
       </div>
 
-      <header className="heading">
+      <header>
         <h1>
           When Hobby
           <br />
-          becomes <span className="passion">Passion</span>
+          become <span className="passion">Passion</span>
         </h1>
       </header>
 
       <section className="about-section">
-        <h2>Hello, there!</h2>
+        <h2>About Me</h2>
         <p>
-          My name is Muhammad Chandra Hasan, I'm Junior Web Developer based on
-          Pinrang, South Sulawesi, Indonesia. My hobby is to learn something new
-          about programming especially web development.
+          My name is Muhammad Chandra Hasan, a Junior Web Developer Based on
+          Pinrang, South Sulawesi, Indonesia. My passion is to learn more about
+          world of programming and my hobby is making websites and web
+          applications.
         </p>
       </section>
 
       <section className="skills-section">
         <h2>My Skills</h2>
         <p>
-          Some of my skills isn't just about Web Development. You may want to
-          explore all of it.
+          I always use my free time to learn something new about programming.
         </p>
 
-        <div className="skills">
-          {my_skills.map((ms, i) => (
-            <SkillCard
-              icon={ms.icon}
-              name={ms.name}
-              star_rating={ms.star_rating}
-              key={i}
-            />
-          ))}
-        </div>
-      </section>
-
-      <section className="contact-section">
-        <h2>Get in touch with me</h2>
-        <p>
-          I'm available for freelancing and remote job. If you need to make
-          website or web application, I'm always ready to help.
-        </p>
-        <p>Just let me know when you need me.</p>
-
-        <div className="contact-form">
-          <form>
-            <input
-              type="email"
-              required
-              className="text-input"
-              placeholder="Your E-mail"
-            />
-            <input
-              type="text"
-              required
-              className="text-input"
-              placeholder="Subject"
-            />
-            <textarea className="textarea-input" placeholder="Your message"></textarea>
-
-            <button type="submit">Send</button>
-          </form>
+        <div className="skill-container">
+          <SkillCard percentage={90} title="Frontend Web Development" />
+          <SkillCard percentage={92} title="Backend Web Development" />
+          <SkillCard percentage={78} title="Web Design" />
+          <SkillCard percentage={82} title="Python Automation" />
+          <SkillCard percentage={75} title="React Native Development" />
+          <SkillCard percentage={41} title="Android Development" />
         </div>
       </section>
     </main>
   );
 }
 
-function SkillCard(props: SkillCardProps) {
-  const getRatingDescription = (rating: number) => {
-    switch (rating) {
-      case 1:
-        return "Beginner";
-      case 2:
-        return "Intermediate";
-      case 3:
-        return "Expert";
-    }
-  };
+function SkillCard({title, percentage}:{percentage: number,title: string}) {
+  const calculateOffset = (p:number) => {
+    const output = 184.78073120117188 * ((100 - p) / 100)
+
+    return output
+  }
 
   return (
-    <div className="skill-card">
-      {props.icon}
+    <div className="skill">
+      <svg xmlns="http://www.w3.org/2000/svg" height="72" width="72">
+        <path
+          className="bg"
+          stroke="#1e293b"
+          d="M 22 70 a 34 34 0 1 1 28 0"
+          fill="none"
+          strokeWidth="4px"
+        />
+        <path
+          className="meter"
+          stroke="#f1f5f9"
+          d="M 22 70 a 34 34 0 1 1 28 0"
+          fill="none"
+          strokeDasharray="184"
+          strokeDashoffset={calculateOffset(percentage)}
+          strokeWidth="4px"
+        />
+        <text x="20" y="44" fill="#f1f5f9">{percentage}%</text>
+      </svg>
 
-      <h5>{props.name}</h5>
-
-      <div className="star-rating">
-        {props.star_rating === 1 && (
-          <>
-            <Icon
-              icon="solar:star-bold"
-              width={24}
-              height={24}
-              color="#FAD516"
-            />
-            <Icon
-              icon="solar:star-bold"
-              width={24}
-              height={24}
-              color="#F1F5F9"
-            />
-            <Icon
-              icon="solar:star-bold"
-              width={24}
-              height={24}
-              color="#F1F5F9"
-            />
-          </>
-        )}
-        {props.star_rating === 2 && (
-          <>
-            <Icon
-              icon="solar:star-bold"
-              width={24}
-              height={24}
-              color="#FAD516"
-            />
-            <Icon
-              icon="solar:star-bold"
-              width={24}
-              height={24}
-              color="#FAD516"
-            />
-            <Icon
-              icon="solar:star-bold"
-              width={24}
-              height={24}
-              color="#F1F5F9"
-            />
-          </>
-        )}
-        {props.star_rating === 3 && (
-          <>
-            <Icon
-              icon="solar:star-bold"
-              width={24}
-              height={24}
-              color="#FAD516"
-            />
-            <Icon
-              icon="solar:star-bold"
-              width={24}
-              height={24}
-              color="#FAD516"
-            />
-            <Icon
-              icon="solar:star-bold"
-              width={24}
-              height={24}
-              color="#FAD516"
-            />
-          </>
-        )}
-      </div>
-
-      <p>{getRatingDescription(props.star_rating)}</p>
+      <h5>{title}</h5>
     </div>
   );
 }
